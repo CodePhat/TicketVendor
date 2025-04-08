77f9eff442c8a7878912c3fe561bccd090757ba0
lepha <lepha@DESKTOP-C33UP11>
2025-04-08 23:23:09 +0700

    Add project files.

---
+++ TicketForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace TicketVendorDemo
+{
+    partial class TicketForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox cmbDestination;
+        private System.Windows.Forms.TextBox txtCard;
+        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.TextBox txtReport;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.cmbDestination = new System.Windows.Forms.ComboBox();
+            this.txtCard = new System.Windows.Forms.TextBox();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbDestination
+            // 
+            this.cmbDestination.FormattingEnabled = true;
+            this.cmbDestination.Location = new System.Drawing.Point(30, 30);
+            this.cmbDestination.Name = "cmbDestination";
+            this.cmbDestination.Size = new System.Drawing.Size(200, 24);
+            this.cmbDestination.TabIndex = 0;
+            // 
+            // txtCard
+            // 
+            this.txtCard.Location = new System.Drawing.Point(30, 70);
+            this.txtCard.Name = "txtCard";
+            this.txtCard.Size = new System.Drawing.Size(200, 22);
+            this.txtCard.TabIndex = 1;
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.Location = new System.Drawing.Point(30, 110);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(200, 30);
+            this.btnPurchase.TabIndex = 2;
+            this.btnPurchase.Text = "Purchase Ticket";
+            this.btnPurchase.UseVisualStyleBackColor = true;
+            this.btnPurchase.Click += new System.EventHandler(this.btnPurchase_Click);
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(30, 160);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(300, 100);
+            this.txtReport.TabIndex = 3;
+            // 
+            // TicketForm
+            // 
+            this.ClientSize = new System.Drawing.Size(380, 280);
+            this.Controls.Add(this.cmbDestination);
+            this.Controls.Add(this.txtCard);
+            this.Controls.Add(this.btnPurchase);
+            this.Controls.Add(this.txtReport);
+            this.Name = "TicketForm";
+            this.Text = "Ticket Vendor Machine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ TicketForm.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TicketVendorDemo
+{
+    public partial class TicketForm : Form
+    {
+        string connStr = "Data Source=.;Initial Catalog=LabDB;Integrated Security=True";
+
+        public TicketForm()
+        {
+            InitializeComponent();
+            LoadDestinations();
+        }
+
+        private void LoadDestinations()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, Name FROM Destinations", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cmbDestination.Items.Add(new ComboBoxItem(reader["Name"].ToString(), reader["Id"].ToString()));
+                }
+            }
+        }
+
+        private void btnPurchase_Click(object sender, EventArgs e)
+        {
+            var selected = cmbDestination.SelectedItem as ComboBoxItem;
+            string cardNum = txtCard.Text;
+            int destId = int.Parse(selected.Value);
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                SqlCommand getPrice = new SqlCommand("SELECT Price FROM Destinations WHERE Id=@id", conn);
+                getPrice.Parameters.AddWithValue("@id", destId);
+                decimal price = (decimal)getPrice.ExecuteScalar();
+
+                SqlCommand insertTxn = new SqlCommand(
+                    "INSERT INTO Transactions (DestinationId, CardNumber, Amount) OUTPUT INSERTED.Id VALUES (@dest, @card, @amount)", conn);
+                insertTxn.Parameters.AddWithValue("@dest", destId);
+                insertTxn.Parameters.AddWithValue("@card", cardNum);
+                insertTxn.Parameters.AddWithValue("@amount", price);
+                int txnId = (int)insertTxn.ExecuteScalar();
+
+                string ticketCode = "TKT" + DateTime.Now.Ticks.ToString().Substring(10);
+                SqlCommand insertTkt = new SqlCommand(
+                    "INSERT INTO Tickets (TransactionId, TicketCode) VALUES (@txn, @code)", conn);
+                insertTkt.Parameters.AddWithValue("@txn", txnId);
+                insertTkt.Parameters.AddWithValue("@code", ticketCode);
+                insertTkt.ExecuteNonQuery();
+
+                txtReport.Text = $"✅ Ticket Purchased!\nDestination: {selected.Text}\nAmount: {price:C}\nTicket Code: {ticketCode}";
+            }
+        }
+
+        public class ComboBoxItem
+        {
+            public string Text { get; }
+            public string Value { get; }
+
+            public ComboBoxItem(string text, string value)
+            {
+                Text = text;
+                Value = value;
+            }
+
+            public override string ToString() => Text;
+        }
+    }
+}
